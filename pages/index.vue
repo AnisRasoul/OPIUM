bd709fbfc1be4763516de83b25de5f7b9a15a9d0
AnisRasou <anis.rasoul@etu.univ-batna2.dz>
2025-03-19 03:39:45 +0100

    adding nuxt with tailwind

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen flex">
+        <h1 class="text-7xl font-bold uppercase">Opium</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
